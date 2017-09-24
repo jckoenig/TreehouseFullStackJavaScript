384dca3a32c3136393a41c93ecfb2f75b04871ed
jckoenig <koenig.jake@gmail.com>
2017-09-24 17:07:53 -0500

    First project 1 upload
    contains quotes array and initial logic

---
+++ Project 01/js/quotes.js
@@ -0,0 +1,51 @@
+var quotes = [
+    {
+        quote: "Be yourself; everyone else is already taken.",
+        source: "Oscar Wilde"
+    },
+    {
+        quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+        source: "Albert Einstein",
+        citation: "(supposedly)"
+    },
+    {
+        quote: "You just can’t beat the person who never gives up",
+        source: "Babe Ruth",
+        citation: "The Rotarian",
+        year: 1940
+    },
+    {
+        quote: "Don’t keep forever on the public road. Leave the beaten track behind occasionally and dive into the woods. You will be certain to find something you have never seen before, and something worth thinking about to occupy your mind. All really big discoveries are the result of thought.",
+        source: "Alexander Graham Bell",
+        citation: "National Geographic",
+        year: 1914
+    },
+    {
+        quote: "Music itself is going to become like running water or electricity",
+        source: "David Bowie",
+        citation: "The New York Times",
+        year: 2002
+    },
+    {
+        quote: "Men are from Earth, women are from Earth. Deal with it.",
+        source: "George Carlin"
+    },
+    {
+        quote: "Think of how stupid the average person is, and realize half of them are stupider than that.",
+        source: "George Carlin"
+    },
+    {
+        quote: "Scratch any cynic and you will find a disappointed idealist.",
+        source: "George Carlin"
+    },
+    {
+        quote: "So many books, so little time.",
+        source: "Frank Zappa"
+    },
+    {
+        quote: "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
+        source: "J.K. Rowling",
+        citation: "Harry Potter and the Goblet of Fire",
+        year: 2000
+    }
+];
--- Project 01/js/script.js
+++ Project 01/js/script.js
@@ -2,3 +2,15 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+//uses random number * length to get proper range
+function getRandomQuote() {
+    return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+//builds print string out of randomly selected quote
+function printQuote() {
+    var selectedQuote = getRandomQuote();
+    message = '<p class="quote">' + selectedQuote.quote + '</p>';
+    message += '<p class="source">' + selectedQuote.source + '</p>';
+    document.getElementById('quote-box').innerHTML = message;
+}
