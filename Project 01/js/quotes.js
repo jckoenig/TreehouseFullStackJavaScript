17538a60b7278af3f30f8694268519f527e9432d
jckoenig <koenig.jake@gmail.com>
2017-09-25 19:10:32 -0500

    Full functionality check in
    Only small flaw is that I can't seem to get the button to stay changed colors...

--- Project 01/js/quotes.js
+++ Project 01/js/quotes.js
@@ -2,60 +2,70 @@ var quotes = [
     {
         quote: "Be yourself; everyone else is already taken.",
         source: "Oscar Wilde",
-        tags: ['humor', 'self help']
+        tags: ['humor', 'self help'],
+        positiveAttribution: false
     },
     {
         quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
         source: "Albert Einstein",
         citation: "(supposedly)",
-        tags: ['humor', 'science']
+        tags: ['humor', 'science'],
+        positiveAttribution: false
     },
     {
         quote: "You just can’t beat the person who never gives up",
         source: "Babe Ruth",
         citation: "The Rotarian",
         year: 1940,
-        tags: ['sports', 'motivation']
+        tags: ['sports', 'motivation'],
+        positiveAttribution: true
     },
     {
-        quote: "Don’t keep forever on the public road. Leave the beaten track behind occasionally and dive into the woods. You will be certain to find something you have never seen before, and something worth thinking about to occupy your mind. All really big discoveries are the result of thought.",
+        quote: "Don’t keep forever on the public road. Leave the beaten track behind occasionally and dive into the woods...All really big discoveries are the result of thought.",
         source: "Alexander Graham Bell",
         citation: "National Geographic",
         year: 1914,
-        tags: ['motivation', 'self help']
+        tags: ['motivation', 'self help'],
+        positiveAttribution: true
     },
     {
         quote: "Music itself is going to become like running water or electricity",
         source: "David Bowie",
         citation: "The New York Times",
         year: 2002,
-        tags: ['music', 'news']
+        tags: ['music', 'news'],
+        positiveAttribution: true
     },
     {
         quote: "Men are from Earth, women are from Earth. Deal with it.",
         source: "George Carlin",
-        tags: ['humor', 'comedian']
+        tags: ['humor', 'comedian'],
+        positiveAttribution: true
     },
     {
         quote: "Think of how stupid the average person is, and realize half of them are stupider than that.",
         source: "George Carlin",
-        tags: ['humor', 'comedian']
+        tags: ['humor', 'comedian'],
+        positiveAttribution: true
     },
     {
         quote: "Scratch any cynic and you will find a disappointed idealist.",
         source: "George Carlin",
-        tags: ['humor', 'comedian']
+        tags: ['humor', 'comedian'],
+        positiveAttribution: true
     },
     {
         quote: "So many books, so little time.",
         source: "Frank Zappa",
-        tags: ['humor', 'self improvement']
+        tags: ['humor', 'self improvement'],
+        positiveAttribution: true
     },
     {
         quote: "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
         source: "J.K. Rowling",
         citation: "Harry Potter and the Goblet of Fire",
         year: 2000,
-        tags: ['book', 'knowledge']
+        tags: ['book', 'knowledge'],
+        positiveAttribution: true
     }
 ];
--- Project 01/js/script.js
+++ Project 01/js/script.js
@@ -1,11 +1,16 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-document.getElementById('loadQuote').addEventListener("click", randomBackgroundColor, false);
-
-//sets an automatic refresh of the quote every 10 seconds if the button isn't clicked (extra credit)
-var intervalID = setInterval(printQuote, 10000);
-
+document.getElementById('loadQuote').addEventListener("click", randomColorsSetter, false);
+var quoteInterval;
+var colorInterval;
+//function to store the multiple things that need to be done in an interval
+function setBothIntervals() {
+    quoteInterval = setInterval(printQuote, 10000);
+    colorInterval = setInterval(randomColorsSetter, 10000);
+}
+//sets an automatic refresh of the quote and color change every 10 seconds if the button isn't clicked on page load (extra credit)
+setBothIntervals();
 //array to hold quotes that have been used so that all quotes will be seen before repeating (extra credit)
 var usedQuotes = [];
 var selectedQuote = {};
@@ -25,48 +30,52 @@ function printQuote() {
     //first check if all quotes have been used already, if so reset usedQuotes
     if (usedQuotes.length === quotes.length) {
         usedQuotes = [];
+        //log to the console when the array is reset (for debug)
+        console.log("All quotes used");
     }
 
     selectedQuote = getUniqueQuote();
 
     message = '<p class="quote">' + selectedQuote.quote + '</p>';
     message += '<p class="source">' + selectedQuote.source;
-
     //check if there's a citation, if so add to message
     if ('citation' in selectedQuote) {
         message += '<span class="citation">' + selectedQuote.citation + '</span>';
     }
-
     //same as above but for year
     if ('year' in selectedQuote) {
         message += '<span class="year">' + selectedQuote.year + '</span>';
     }
 
-    //close the paragraph tag whether or not citation/year were added
+    //close the paragraph tag and apply HTML change whether or not citation/year were added
     message += '</p>';
-
     document.getElementById('quote-box').innerHTML = message;
 
     //log quote to console to track use (extra credit)
     console.log(selectedQuote.quote);
+
     //add quote to usedQuotes to avoid using it again for now
     usedQuotes.push(selectedQuote);
-    //reset the interval so that it doesn't immediately pick another quote
-    clearInterval(intervalID);
-    intervalID = setInterval(printQuote, 10000);
+
+    //reset the intervals so that they don't immediately pick another quote
+    clearInterval(quoteInterval);
+    clearInterval(colorInterval);
+    setBothIntervals();
 }
 
 //generate a random quote by trapping the quote generation in a loop until it is unique (extra credit)
 function getUniqueQuote(selectedQuote) {
     var uniqueQuote = getRandomQuote();
 
-    while (uniqueQuote in usedQuotes) {
+    while (usedQuotes.includes(uniqueQuote)) {
         uniqueQuote = getRandomQuote();
     }
     return uniqueQuote;
 }
 
 //function to change background color when called to a randomly generated RGB value (extra credit)
-function randomBackgroundColor() {
-    document.body.style.backgroundColor = "rgb(" + getColorNumber() + ", " + getColorNumber() + ", " + getColorNumber() + ")";
+function randomColorsSetter() {
+    var colorString = "rgb(" + getColorNumber() + ", " + getColorNumber() + ", " + getColorNumber() + ")";
+    document.body.style.backgroundColor = colorString;
+    document.getElementById("loadQuote").setAttribute("background-color", colorString);
 }
